013e89a1e5945c25929acca166f6736f26db9396
Robin Vinod <robinvvinod@gmail.com>
2020-08-18 11:44:23 +0800

    Added in place quick sort

--- Sort/quickSort.py
+++ Sort/quickSort.py
@@ -2,6 +2,7 @@
 # Worst case --> O(n^2)
 # Average case --> O(nlogn)
 
+
 def quickSort(L):
 
     less = []
@@ -19,3 +20,34 @@ def quickSort(L):
             more.append(L[i])
 
     return quickSort(less) + [pivot] + quickSort(more)
+
+
+def partition(array, start, end):
+    pivot = array[start]
+    low = start + 1
+    high = end
+
+    while True:
+        while low <= high and array[high] >= pivot:
+            high -= 1
+
+        while low <= high and array[low] <= pivot:
+            low += 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[start]
+
+    return high
+
+
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+
+    p = partition(array, start, end)
+    quick_sort(array, start, p - 1)
+    quick_sort(array, p + 1, end)
